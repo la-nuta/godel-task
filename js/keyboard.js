a99d619e5c75b4224cbc45c19c5ea1c05242350f
Anna Marfel <anna.marfel@gmail.com>
2018-05-02 05:07:10 +0300

    Add keyboard missing symbols Refactor js: language change, etc

--- js/keyboard.js
+++ js/keyboard.js
@@ -9,16 +9,18 @@ class Keyboard {
             16, 'x', 90, 88, 67, 86, 66, 78, 77, 188, 190, 191, 16, 'x',
             17, 'x', 18, 32, 'alt gr', 37, 39, 38, 40, 'x'];
 
-        this.ciryllicKeyboard = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', String.fromCharCode(9003), String.fromCharCode(127303), String.fromCharCode(215),
-            String.fromCharCode(8677), 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', String.fromCharCode(9166), String.fromCharCode(10138), 'Abc',
-            String.fromCharCode(8682), 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', '\\', '??????',
-            String.fromCharCode(8679), '<', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', String.fromCharCode(8679), '123',
-            'Ctrl', String.fromCharCode(127760), 'Alt', '', 'Alt Gr', String.fromCharCode(8592), String.fromCharCode(8594), String.fromCharCode(8593), String.fromCharCode(8595), '?????'];
-        this.latinKeyboard = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', String.fromCharCode(9003), String.fromCharCode(127303), String.fromCharCode(215),
-            String.fromCharCode(8677), 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', String.fromCharCode(9166), String.fromCharCode(10138), 'Abc',
-            String.fromCharCode(8682), 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '\\', '??????',
-            String.fromCharCode(8679), '<', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', String.fromCharCode(8679), '123',
-            'Ctrl', String.fromCharCode(127760), 'Alt', '', 'Alt Gr', String.fromCharCode(8592), String.fromCharCode(8594), String.fromCharCode(8593), String.fromCharCode(8595), '?????'];
+        this.ciryllicKeyboard = ['й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з',
+            'х', 'ъ', 'next', 'next', 'next', 'next', 'ф', 'ы', 'в', 'а',
+            'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'next', 'next', 'next',
+            'next', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю',
+            '.'
+        ];
+        this.latinKeyboard = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
+            '[', ']', 'next', 'next', 'next', 'next', 'a', 's', 'd', 'f',
+            'g', 'h', 'j', 'k', 'l', ';', '\'', 'next', 'next', 'next',
+            'next', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.',
+            '/'
+        ];
 
         this.input = document.forms.keyboardForm.keyboardInput;
 
@@ -42,15 +44,44 @@ class Keyboard {
         } else {
             this.el.blur();
         }
+
         if (e.target.classList.contains('key')) {
             this.cleanCurrent();
             e.target.classList.add('current');
+
+            if (e.target.classList.contains('key30')) {
+                let enterKey = document.querySelector('.key30-1');
+                enterKey.classList.add('current');
+            }
         }
-        if (e.target.classList.contains('printable')) {
+        if (e.target.closest('div').classList.contains('key')) {
+            e.target.closest('div').classList.add('current');
+        }
+
+        if (e.target.classList.contains('symbol')) {
             this.input.value = `${this.input.value}${e.target.textContent}`;
         }
         if (e.target.classList.contains('key16')) {
             this.destroy();
+        }
+
+        if (e.target.classList.contains('key33')) {
+            let keys = Array.from(document.querySelectorAll('.key')).slice(17, 59);
+            keys.splice(12, 1);
+
+            if (keys[0].textContent === this.latinKeyboard[0]) {
+                for (let i = 0; i < keys.length; i++) {
+                    if (this.ciryllicKeyboard[i] !== 'next') {
+                        keys[i].textContent = this.ciryllicKeyboard[i];
+                    }
+                }
+            } else if (keys[0].textContent === this.ciryllicKeyboard[0]) {
+                for (let i = 0; i < keys.length; i++) {
+                    if (this.latinKeyboard[i] !== 'next') {
+                        keys[i].textContent = this.latinKeyboard[i];
+                    }
+                }
+            }
         }
     }
 
@@ -69,6 +100,7 @@ class Keyboard {
 
     destroy() {
         this.el.remove();
+        document.querySelector('.description').remove();
         document.forms.keyboardForm.remove();
         this.el.removeEventListener('onClick', this);
         this.el.removeEventListener('keydown', this);
---
+++ readme.md
@@ -0,0 +1,13 @@
+## To build the project with Webstorm:
+
+```
+npm i less -D
+npm i gulp -D
+```
+
+* Webstorm --> Preferencies --> Tools --> File Watchers --> 
+add Less and add “Tool to run on changes” “Program” path-to-project/node_modules/less/bin/lessc
+
+```
+npm i gulp-less
+```
